005c0d411c7a41b6d170757f1454ff80d39536dd
Guilherme Ortiz <code@guilhermeortiz.dev>
2021-08-15 21:36:24 -0300

    Create installerScript.sh

---
+++ installerScript.sh
@@ -0,0 +1,60 @@
+echo "---> Update and Upgrade"
+sudo apt-get update -y
+sudo apt-get upgrade -y
+echo "---> vim"
+sudo apt-get install vim -y
+echo "---> curl"
+sudo apt-get install curl -y
+echo "---> git"
+sudo apt-get install git -y
+echo "---> gdebi"
+sudo apt-get install gdebi -y
+echo "---> brew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "---> terminator"
+sudo apt-get install terminator -y
+echo "---> dotnet"
+wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update -y&& \
+  sudo apt-get install -y dotnet-sdk-5.0
+echo "---> docker"
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+echo "---> AWS"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+brew tap aws/tap -y
+brew install aws-sam-cli -y
+echo "---> Language Config"
+## Fix Cedilha - https://www.danielkossmann.com/ajeitando-cedilha-errado-ubuntu-linux/
+echo 'GTK_IM_MODULE=cedilla' | sudo tee -a /etc/environment
+echo "---> discord"
+wget -O ~/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
+sudo gdebi ~/discord.deb -y
+echo "---> vsCode"
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+sudo apt install apt-transport-https -y
+sudo apt update -y
+sudo apt install code -y
+echo "---> zsh"
+sudo apt-get install zsh -y
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
